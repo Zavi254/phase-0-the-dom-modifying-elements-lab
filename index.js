a834df219323389818afa1d138abc773bb1ed1aa
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-03 20:44:12 +0300

    commit tree eab9c2fd66808e87d2893a53f4ce89876e22dae9

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainRemoved = document.getElementById('main').remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Steven Otieno is the champion"
